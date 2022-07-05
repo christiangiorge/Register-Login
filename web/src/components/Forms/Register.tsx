92c2f9c70d73b11bb7e01cb8b5586f193653505b
Christian Giorge <chrisgiorgeas@gmail.com>
2022-07-04 22:41:47 -0300

    Validar cadastro de senha
    Caso de uso para validar senha pelo front-end

--- web/src/components/Forms/Register.tsx
+++ web/src/components/Forms/Register.tsx
@@ -1,33 +1,43 @@
 import { useState } from "react"
 
-import { validatePassword } from '../../utils/regex';
+import { regexLength, regexNumber, regexUppercase, regexSpecial } from '../../utils/regex';
 
 import notRed from "../../assets/not-red.svg";
 import checkGreen from "../../assets/check-green.svg";
 import { Password } from "phosphor-react";
 
 export function Register() {
-    
-    
 
-    const [password, setPassword] = useState({
-      validatePassword: false
+    const [validatePassword, setValidatePassword] = useState({
+        regexLength: false,
+        regexNumber: false,
+        regexUppercase: false,
+        regexSpecial: false,
     })
 
     const secureText = (passwordInput: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword({
-            validatePassword: validatePassword.test(passwordInput.target.value)
+        
+        setValidatePassword({
+            regexLength: regexLength.test(passwordInput.target.value),
+            regexNumber: regexNumber.test(passwordInput.target.value),
+            regexUppercase: regexUppercase.test(passwordInput.target.value),
+            regexSpecial: regexSpecial.test(passwordInput.target.value),
+        })
+    }
+
+    const [matchPassword, setMatchPassword] = useState("")
+
+    const secureMatchPassword = (matchPassword: React.ChangeEvent<HTMLInputElement>) => {
+        setMatchPassword({
+           matchPassword
         })
     }
 
 
-    // const validatePassword = (password: String) => {
-    //     const regexUppercase = RegExp(/^(?=.*[A-Z]).+$/)
-    //     const regexLowercase = RegExp(/^(?=.*[a-z]).+$/)
-    //     const regexNumber = RegExp(/^(?=.*[0-9]).+$/)
-    //     const regexSpecial = RegExp(/^(?=.*[$&+,:;=?@#|'<>.-^*()%!]).+$/)
-    //     const length = password.length >= 8
-    // } 
+    
+
+
+
 
     return (
         <div className="flex flex-col items-center">
@@ -35,20 +45,18 @@ export function Register() {
 
             <label>
                 <form className="flex flex-col items-center">
-                    <input type="text" placeholder="Your Name Complete" className="block peer rounded-[5px] border-[#AEBBCD] outline-none w-[25rem] mb-5"/>
+                    <input type="text" placeholder="Your Name Complete" className="block peer rounded-[5px] border-[#AEBBCD] outline-none w-[25rem] mb-5 "/>
 
-                    <input type="email" placeholder="Confirm your Email ID" className="block peer rounded-[5px] border-[#AEBBCD] outline-none w-[25rem] mb-5"/>
+                    <input type="email" placeholder="Confirm your Email ID" className="block peer rounded-[5px] border-[#AEBBCD] outline-none w-[25rem] mb-5 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"/>
 
-                    <input type="password" placeholder="New Password" onChange={(passwordInput) => secureText(passwordInput)} className="block peer rounded-[5px] border-[#AEBBCD] outline-none w-[25rem] mb-5"/>
+                    <input type="password" placeholder="New Password" onChange={(passwordInput) => secureText(passwordInput)} className="block rounded-[5px] border-[#AEBBCD] outline-none w-[25rem] mb-5"/>
                     
+                    <input type="password" placeholder="Confirm Password" onChange={(matchPassword) => secureMatchPassword=(matchPassword)} className="block peer rounded-[5px] border-[#AEBBCD] outline-none w-[25rem] mb-5"/>
 
-                    <input type="password" placeholder="Confirm Password" className="block peer rounded-[5px] border-[#AEBBCD] outline-none w-[25rem] mb-5"/>
-
-                    <button className="rounded-full bg-[#3D5FD9] text-[#F5F7FF] w-[25rem] p-3 hover:bg-[#2347C5] mb-5">SIGN IN</button>
+                    <button className="rounded-full bg-[#3D5FD9] text-[#F5F7FF] w-[25rem] p-3 hover:bg-[#2347C5] mb-5">SIGN UP</button>
 
                     <p className="text-[#5473E3] mb-5">Already have an account ? < a href="#" className="hover:text-[#2347C5] hover">Sign in</a></p>
                 
-                   
                 
                 </form>
                 
@@ -57,23 +65,23 @@ export function Register() {
                     <label className="text-[#404B5A]">Senha deve conter:</label>
                     
                     <div className="mt-2 ">
-                        <img  src={notRed} className="inline-block mr-2"/>
+                        <img  src={validatePassword.regexLength ? checkGreen : notRed} className="inline-block mr-2" />
                         <p className="inline-block">Minímo 8 caracteres;</p>
                     </div>
                     
                     <div>
-                            <img src={password.validatePassword ? checkGreen : notRed} className="inline-block mr-2"/>
+                        <img src={validatePassword.regexNumber ? checkGreen : notRed } className="inline-block mr-2"/>
                         <p className="inline-block">Pelo menos um número;</p>
                         
                     </div>
                         
                     <div>
-                        <img src={notRed} className="inline-block mr-2"/>
+                        <img src={validatePassword.regexUppercase ? checkGreen : notRed} className="inline-block mr-2"/>
                         <p className="inline-block">Pelo menos uma letra maiuscula;</p>
                     </div>
                     
                     <div>
-                        <img src={notRed} className="inline-block mr-2"/>
+                        <img src={validatePassword.regexSpecial ? checkGreen : notRed} className="inline-block mr-2"/>
                         <p className="inline-block">Pelo menos um caracter especial;</p>
                     </div>
                 
--- web/src/utils/regex.tsx
+++ web/src/utils/regex.tsx
@@ -1,3 +1,15 @@
-export const validatePassword = new RegExp(
-   "^(?=.*[0-9]).+$" 
-)
+export const regexLength = new RegExp("^.{8,}$")
+export const regexNumber = new RegExp("^(?=.*[0-9]).+$")
+export const regexUppercase = new RegExp("^(?=.*[A-Z]).+$")
+export const regexSpecial = new RegExp("^(?=.*[$&+,:;=?@#|'`~<>_{}.^*()%!]).+$")
+
+
+   
+
+
+
+
+
+    // const validatePassword = (password: String) => {
+    //     const length = password.length >= 8
+    // } 
